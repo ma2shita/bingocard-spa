dee959f845f9f72284c2826cdb7f45117d1ba057
Kohei MATSUSHITA <ma2shita+git@ma2shita.jp>
2021-03-03 23:39:43 +0900

    DEMO

--- config.js
+++ config.js
@@ -1,314 +1,308 @@
 const img_src_prefix = 'assets/';
 const pickup_wait = 25;
-
-/*
- $ ruby -rjson -e 'puts Dir.glob("*.png").shuffle.map.with_index.inject({}){|h, (i,n)|h[n]={src:i, text:i.split("-")[1].split(".")[0]};h}.to_json
-
- and Edit
-*/
 const bingo_items = {
   "0": {
-    "src": "SISS002-SORACOM Platform B.png",
+    "src": "SISS002.png",
     "text": "SORACOM Platform B"
   },
   "1": {
-    "src": "SISS010-SORACOM Door.png",
-    "text": "SORACOM Door"
-  },
-  "2": {
-    "src": "SIS0031-セキュリティC.png",
-    "text": "セキュリティC"
-  },
-  "3": {
-    "src": "SIS0114-トラック.png",
-    "text": "トラック"
-  },
-  "4": {
-    "src": "SIS0115-バス.png",
-    "text": "バス"
-  },
-  "5": {
-    "src": "SIS0093-LoRa ゲートウェイ A.png",
-    "text": "LoRa ゲートウェイ A"
-  },
-  "6": {
-    "src": "SISS015-SORACOM Inventory.png",
-    "text": "SORACOM Inventory"
-  },
-  "7": {
-    "src": "SIS0090-SBR A.png",
-    "text": "SBR A"
-  },
-  "8": {
-    "src": "SIS0118-オートバイ.png",
-    "text": "オートバイ"
-  },
-  "9": {
-    "src": "SIS0066-矢印C.png",
-    "text": "矢印C"
-  },
-  "10": {
-    "src": "SIS0124-ブルドーザー.png",
-    "text": "ブルドーザー"
-  },
-  "11": {
-    "src": "SISS014-SORACOM Harvest.png",
-    "text": "SORACOM Harvest"
-  },
-  "12": {
-    "src": "SISS004-Air SIM B.png",
-    "text": "Air SIM B"
-  },
-  "13": {
-    "src": "SIS0096-LoRa モジュール B.png",
-    "text": "LoRa モジュール B"
-  },
-  "14": {
-    "src": "SISS018-SORACOM Air.png",
-    "text": "SORACOM Air"
-  },
-  "15": {
-    "src": "SIS0047-夜間.png",
-    "text": "夜間"
-  },
-  "16": {
-    "src": "SIS0059-メール.png",
-    "text": "メール"
-  },
-  "17": {
-    "src": "SISS006-SORACOM Canal.png",
-    "text": "SORACOM Canal"
-  },
-  "18": {
-    "src": "SIS0109-Sigfox モジュール B.png",
-    "text": "Sigfox モジュール B"
-  },
-  "19": {
-    "src": "SIS0013-監視カメラ.png",
-    "text": "監視カメラ"
-  },
-  "20": {
-    "src": "SISS016-SORACOM Inventory client.png",
-    "text": "SORACOM Inventory client"
-  },
-  "21": {
-    "src": "SIS0099-VPG.png",
-    "text": "VPG"
-  },
-  "22": {
-    "src": "SISS007-SORACOM Direct.png",
-    "text": "SORACOM Direct"
-  },
-  "23": {
-    "src": "SISS009-SORACOM Funnel.png",
-    "text": "SORACOM Funnel"
-  },
-  "24": {
-    "src": "SIS0061-再読み込み.png",
-    "text": "再読み込み"
-  },
-  "25": {
-    "src": "SISS005-SORACOM Beam.png",
-    "text": "SORACOM Beam"
-  },
-  "26": {
-    "src": "SIS0122-ショベルカー.png",
-    "text": "ショベルカー"
-  },
-  "27": {
-    "src": "SIS0126-トラクター.png",
-    "text": "トラクター"
-  },
-  "28": {
-    "src": "SIS0121-ヘリコプター.png",
-    "text": "ヘリコプター"
-  },
-  "29": {
-    "src": "SIS0101-相互接続網.png",
-    "text": "相互接続網"
-  },
-  "30": {
-    "src": "SIS0042-送信.png",
-    "text": "送信"
-  },
-  "31": {
-    "src": "SIS0125-ダンプカー.png",
-    "text": "ダンプカー"
-  },
-  "32": {
-    "src": "SIS0128-ボート.png",
-    "text": "ボート"
-  },
-  "33": {
-    "src": "SISS017-SORACOM Junction.png",
-    "text": "SORACOM Junction"
-  },
-  "34": {
-    "src": "SIS0058-グラフ.png",
-    "text": "グラフ"
-  },
-  "35": {
-    "src": "SIS0107-Beer.png",
-    "text": "Beer"
-  },
-  "36": {
-    "src": "SIS0004-基地局.png",
-    "text": "基地局"
-  },
-  "37": {
-    "src": "SIS0132-スペースシャトル.png",
-    "text": "スペースシャトル"
-  },
-  "38": {
-    "src": "SIS0003-交換局.png",
+    "src": "SIS0003.png",
     "text": "交換局"
   },
-  "39": {
-    "src": "SIS0050-認証.png",
-    "text": "認証"
+  "2": {
+    "src": "SIS0004.png",
+    "text": "基地局"
   },
-  "40": {
-    "src": "SIS0046-日中.png",
-    "text": "日中"
-  },
-  "41": {
-    "src": "SIS0057-ビックデータ.png",
-    "text": "ビックデータ"
-  },
-  "42": {
-    "src": "SIS0102-Safe.png",
-    "text": "Safe"
-  },
-  "43": {
-    "src": "SIS0134-衛星.png",
-    "text": "衛星"
-  },
-  "44": {
-    "src": "SIS0123-クレーン.png",
-    "text": "クレーン"
-  },
-  "45": {
-    "src": "SIS0131-気球.png",
-    "text": "気球"
-  },
-  "46": {
-    "src": "SIS0112-自動車.png",
-    "text": "自動車"
-  },
-  "47": {
-    "src": "SIS0113-タクシー.png",
-    "text": "タクシー"
-  },
-  "48": {
-    "src": "SIS0009-スマホ.png",
-    "text": "スマホ"
-  },
-  "49": {
-    "src": "SIS0016-基板.png",
-    "text": "基板"
-  },
-  "50": {
-    "src": "SIS0092-Global.png",
-    "text": "Global"
-  },
-  "51": {
-    "src": "SIS0035-コード.png",
-    "text": "コード"
-  },
-  "52": {
-    "src": "SIS0091-SBR B.png",
-    "text": "SBR B"
-  },
-  "53": {
-    "src": "SIS0070-システム処理C.png",
-    "text": "システム処理C"
-  },
-  "54": {
-    "src": "SIS0133-UFO.png",
-    "text": "UFO"
-  },
-  "55": {
-    "src": "SIS0129-船.png",
-    "text": "船"
-  },
-  "56": {
-    "src": "SIS0127-除雪車.png",
-    "text": "除雪車"
-  },
-  "57": {
-    "src": "SIS0055-コントローラブル.png",
-    "text": "コントローラブル"
-  },
-  "58": {
-    "src": "SIS0105-Connect B.png",
-    "text": "Connect B"
-  },
-  "59": {
-    "src": "SIS0116-電車.png",
-    "text": "電車"
-  },
-  "60": {
-    "src": "SIS0119-自転車.png",
-    "text": "自転車"
-  },
-  "61": {
-    "src": "SISS011-SORACOM Gate.png",
-    "text": "SORACOM Gate"
-  },
-  "62": {
-    "src": "SIS0025-サーバーC.png",
-    "text": "サーバーC"
-  },
-  "63": {
-    "src": "SIS0014-ドローン.png",
-    "text": "ドローン"
-  },
-  "64": {
-    "src": "SIS0120-飛行機.png",
-    "text": "飛行機"
-  },
-  "65": {
-    "src": "SIS0038-設定C.png",
-    "text": "設定C"
-  },
-  "66": {
-    "src": "SIS0130-タンカー.png",
-    "text": "タンカー"
-  },
-  "67": {
-    "src": "SIS0048-認証キー.png",
-    "text": "認証キー"
-  },
-  "68": {
-    "src": "SIS0087-通信経路（分岐）.png",
-    "text": "通信経路（分岐）"
-  },
-  "69": {
-    "src": "SIS0053-API.png",
-    "text": "API"
-  },
-  "70": {
-    "src": "SIS0045-時間.png",
-    "text": "時間"
-  },
-  "71": {
-    "src": "SISS008-SORACOM Endorse.png",
-    "text": "SORACOM Endorse"
-  },
-  "72": {
-    "src": "SISS013-Air SIM for Global B.png",
-    "text": "Air SIM for Global B"
-  },
-  "73": {
-    "src": "SIS0027-ユーザーA.png",
-    "text": "ユーザーA"
-  },
-  "74": {
-    "src": "SIS0008-LAPTOP.png",
+  "3": {
+    "src": "SIS0008.png",
     "text": "LAPTOP"
   },
-  "75": {
-    "src": "SIS0117-原付.png",
+  "4": {
+    "src": "SIS0013.png",
+    "text": "監視カメラ"
+  },
+  "5": {
+    "src": "SIS0016.png",
+    "text": "基板"
+  },
+  "6": {
+    "src": "SIS0017.png",
+    "text": "ドングル"
+  },
+  "7": {
+    "src": "SIS0021.png",
+    "text": "コントローラーB"
+  },
+  "8": {
+    "src": "SIS0025.png",
+    "text": "サーバーC"
+  },
+  "9": {
+    "src": "SIS0026.png",
+    "text": "データベース"
+  },
+  "10": {
+    "src": "SIS0027.png",
+    "text": "ユーザーA"
+  },
+  "11": {
+    "src": "SIS0035.png",
+    "text": "コード"
+  },
+  "12": {
+    "src": "SIS0040.png",
+    "text": "監視"
+  },
+  "13": {
+    "src": "SIS0042.png",
+    "text": "送信"
+  },
+  "14": {
+    "src": "SIS0045.png",
+    "text": "時間"
+  },
+  "15": {
+    "src": "SIS0052.png",
+    "text": "ブロック（鍵あり）"
+  },
+  "16": {
+    "src": "SIS0056.png",
+    "text": "SDK"
+  },
+  "17": {
+    "src": "SIS0070.png",
+    "text": "システム処理C"
+  },
+  "18": {
+    "src": "SIS0087.png",
+    "text": "通信経路（分岐）"
+  },
+  "19": {
+    "src": "SIS0090.png",
+    "text": "SBR A"
+  },
+  "20": {
+    "src": "SIS0091.png",
+    "text": "SBR B"
+  },
+  "21": {
+    "src": "SIS0094.png",
+    "text": "LoRa ゲートウェイ B"
+  },
+  "22": {
+    "src": "SIS0107.png",
+    "text": "Beer"
+  },
+  "23": {
+    "src": "SIS0113.png",
+    "text": "タクシー"
+  },
+  "24": {
+    "src": "SIS0116.png",
+    "text": "電車"
+  },
+  "25": {
+    "src": "SIS0117.png",
     "text": "原付"
+  },
+  "26": {
+    "src": "SIS0123.png",
+    "text": "クレーン"
+  },
+  "27": {
+    "src": "SIS0132.png",
+    "text": "スペースシャトル"
+  },
+  "28": {
+    "src": "SIS0133.png",
+    "text": "UFO"
+  },
+  "29": {
+    "src": "SIS0134.png",
+    "text": "衛星"
+  },
+  "30": {
+    "src": "SIS0137.png",
+    "text": "Air eSIM B"
+  },
+  "31": {
+    "src": "SIS0150.png",
+    "text": "Wio LTE"
+  },
+  "32": {
+    "src": "SIS0156.png",
+    "text": "温度計"
+  },
+  "33": {
+    "src": "SIS0162.png",
+    "text": "機械学習モデルA"
+  },
+  "34": {
+    "src": "SIS0166.png",
+    "text": "グラフC"
+  },
+  "35": {
+    "src": "SIS0176.png",
+    "text": "ルーターC"
+  },
+  "36": {
+    "src": "SIS0181.png",
+    "text": "エクスカリバー"
+  },
+  "37": {
+    "src": "SIS0182.png",
+    "text": "スライム"
+  },
+  "38": {
+    "src": "SIS0183.png",
+    "text": "住居"
+  },
+  "39": {
+    "src": "SIS0185.png",
+    "text": "工場 A"
+  },
+  "40": {
+    "src": "SIS0188.png",
+    "text": "自動販売機"
+  },
+  "41": {
+    "src": "SIS0189.png",
+    "text": "踏切"
+  },
+  "42": {
+    "src": "SIS0192.png",
+    "text": "発電機"
+  },
+  "43": {
+    "src": "SIS0193.png",
+    "text": "PLC"
+  },
+  "44": {
+    "src": "SIS0195.png",
+    "text": "7セグパネル"
+  },
+  "45": {
+    "src": "SIS0197.png",
+    "text": "ロボット"
+  },
+  "46": {
+    "src": "SIS0198.png",
+    "text": "ベルトコンベア"
+  },
+  "47": {
+    "src": "SIS0201.png",
+    "text": "マップ"
+  },
+  "48": {
+    "src": "SIS0207.png",
+    "text": "不届き者"
+  },
+  "49": {
+    "src": "SIS0209.png",
+    "text": "S+Camera B"
+  },
+  "50": {
+    "src": "SIS0211.png",
+    "text": "VPG Type-D"
+  },
+  "51": {
+    "src": "SIS0214.png",
+    "text": "S+Smart Notification Service"
+  },
+  "52": {
+    "src": "SIS0215.png",
+    "text": "GPS マルチユニット SORACOM Edition"
+  },
+  "53": {
+    "src": "SISS004.png",
+    "text": "Air SIM B"
+  },
+  "54": {
+    "src": "SISS005.png",
+    "text": "SORACOM Beam"
+  },
+  "55": {
+    "src": "SISS006.png",
+    "text": "SORACOM Canal"
+  },
+  "56": {
+    "src": "SISS007.png",
+    "text": "SORACOM Direct"
+  },
+  "57": {
+    "src": "SISS008.png",
+    "text": "SORACOM Endorse"
+  },
+  "58": {
+    "src": "SISS009.png",
+    "text": "SORACOM Funnel"
+  },
+  "59": {
+    "src": "SISS010.png",
+    "text": "SORACOM Door"
+  },
+  "60": {
+    "src": "SISS011.png",
+    "text": "SORACOM Gate"
+  },
+  "61": {
+    "src": "SISS012.png",
+    "text": "Air SIM for Global A"
+  },
+  "62": {
+    "src": "SISS014.png",
+    "text": "SORACOM Harvest"
+  },
+  "63": {
+    "src": "SISS015.png",
+    "text": "SORACOM Inventory"
+  },
+  "64": {
+    "src": "SISS016.png",
+    "text": "SORACOM Inventory client"
+  },
+  "65": {
+    "src": "SISS017.png",
+    "text": "SORACOM Junction"
+  },
+  "66": {
+    "src": "SISS018.png",
+    "text": "SORACOM Air"
+  },
+  "67": {
+    "src": "SISS019.png",
+    "text": "SORACOM Krypton"
+  },
+  "68": {
+    "src": "SISS020.png",
+    "text": "SORACOM Krypton key"
+  },
+  "69": {
+    "src": "SISS021.png",
+    "text": "SORACOM Lagoon"
+  },
+  "70": {
+    "src": "SISS022.png",
+    "text": "SORACOM Funk"
+  },
+  "71": {
+    "src": "SISS023.png",
+    "text": "SORACOM Mosaic"
+  },
+  "72": {
+    "src": "SISS024.png",
+    "text": "SORACOM Napter"
+  },
+  "73": {
+    "src": "SISS025.png",
+    "text": "SORACOM Orbit"
+  },
+  "74": {
+    "src": "SISS026.png",
+    "text": "SORACOM Peek"
+  },
+  "75": {
+    "src": "SIS0001.png",
+    "text": "Cloud A"
   }
 }
